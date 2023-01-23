0f065431a2bf472e8f27573af8ad09b9b169ba20
Sam Hamilton <sam@4ds.co.uk>
2023-01-23 10:33:21 +0000

    Add config for car park capacity

---
+++ config/carpark.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Maximum Capacity
+    |--------------------------------------------------------------------------
+    |
+    | This value is the maximum spaces that the carpark has.
+    |
+    */
+
+    'capacity' => env('CAR_PARK_CAPACITY', 10),
+];
