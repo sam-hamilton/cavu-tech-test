4e2846471a070dd4e6ebbd93ed38a3071b3a6b61
Sam Hamilton <sam@4ds.co.uk>
2023-01-23 12:57:45 +0000

    Enable bookings show

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -37,9 +37,9 @@ class BookingController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Booking $booking)
     {
-        //
+        return $booking->load('reservations');
     }
 
     /**
