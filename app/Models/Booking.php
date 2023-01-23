bcecfee3c67b918673d93f2a709346c42a151362
Sam Hamilton <sam@4ds.co.uk>
2023-01-23 10:29:34 +0000

    add booking model

---
+++ app/Models/Booking.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'from' => 'date',
+        'to' => 'date',
+        'payment' => 'decimal:2',
+    ];
+
+    /**
+     * Get payment for humans.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function payment(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value == 0 ? 0 : $value / 100,
+        );
+    }
+
+    /**
+     * Get the user associated with the booking.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the reservations associated with the booking.
+     */
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+}
