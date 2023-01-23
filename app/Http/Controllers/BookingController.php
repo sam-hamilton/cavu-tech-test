6dff5ad712fc8be9ff2c580dc6fcd5e5599832ae
Sam Hamilton <sam@4ds.co.uk>
2023-01-23 12:49:49 +0000

    Remove create and edit from Booking controller
    Not needed for API's

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -21,16 +21,6 @@ class BookingController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -48,17 +38,6 @@ class BookingController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
